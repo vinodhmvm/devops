736d5205c9b227a0814dab8fc5c7196d25b7da21
Vinodh Kumar M <vinodhmvm@gmail.com>
2018-12-22 14:20:33 +0530

    test

--- pipeline/amicreate.groovy
+++ pipeline/amicreate.groovy
@@ -1,11 +1,6 @@
 def stack_aws_id = ''
 def stack_aws_secret = ''
 def stack_aws_cred_name = ''
-def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
-    com.cloudbees.jenkins.plugins.awscredentials.AWSCredentialsImpl.class,
-    Jenkins.instance
-)
-def c = creds.findResult { it.id == id ? it : null }
 node (label: 'jenkinsslave') {
     if (env.Environment == "prod") {
         aws_cred_name = 'prod_aws_access_credentials'

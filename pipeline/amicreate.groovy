53457f53e9b0076a5e2d20fe56468fdbd3c4ecee
Vinodh Kumar M <vinodhmvm@gmail.com>
2018-12-22 13:53:21 +0530

    codeedit

--- pipeline/amicreate.groovy
+++ pipeline/amicreate.groovy
@@ -36,53 +36,53 @@ node (label: 'jenkinsslave') {
             sh ('ls -ltr')
         }
         
-        dir("Packer/Centos7") {
-            def var_file = ""
-            configFileProvider([configFile(fileId: 'packer-var-v1', variable: 'VARIABLE_FILE')]) {
-                sh "cat ${env.VARIABLE_FILE} > variables.json"
-                sh "cat variables.json"
-                sh "sed -i 's/PREFIX/${InstanceType}/g' variables.json"
-                sh "sed -i 's/ENV/${Environment}/g' variables.json"
-                sh "cat variables.json"
-            }
+ //       dir("Packer/Centos7") {
+//            def var_file = ""
+//           configFileProvider([configFile(fileId: 'packer-var-v1', variable: 'VARIABLE_FILE')]) {
+//                sh "cat ${env.VARIABLE_FILE} > variables.json"
+//                sh "cat variables.json"
+//                sh "sed -i 's/PREFIX/${InstanceType}/g' variables.json"
+ //               sh "sed -i 's/ENV/${Environment}/g' variables.json"
+ //               sh "cat variables.json"
+ //           }
             
-            stage('Plan AMI') {
-                // Mark the code build 'plan'....
-                sh """(
-                    packer.io validate -var-file=./variables.json  -var 'aws_access_key=${AWS_ID}' -var 'aws_secret_key=${AWS_KEY}' packer.json; echo \$? > status 
-                )"""
-                def exitCode = readFile('status').trim()
+ //           stage('Plan AMI') {
+ //               // Mark the code build 'plan'....
+ //               sh """(
+ //                   packer.io validate -var-file=./variables.json  -var 'aws_access_key=${AWS_ID}' -var 'aws_secret_key=${AWS_KEY}' packer.json; echo \$? > status 
+  //              )"""
+  //              def exitCode = readFile('status').trim()
                 
-                echo "Packer AMI Plan Exit Code: ${exitCode}"
-                if (exitCode == "0") {
-                    currentBuild.result = 'SUCCESS'
-                }
-                if (exitCode == "1") {
-                    println "AMI Plan Failed: ${env.JOB_NAME} - ${env.BUILD_NUMBER}"
-                    currentBuild.result = 'FAILURE'
-                }
-            }
+ //               echo "Packer AMI Plan Exit Code: ${exitCode}"
+  //              if (exitCode == "0") {
+  //                  currentBuild.result = 'SUCCESS'
+  //              }
+  //              if (exitCode == "1") {
+  //                  println "AMI Plan Failed: ${env.JOB_NAME} - ${env.BUILD_NUMBER}"
+  //                  currentBuild.result = 'FAILURE'
+  //              }
+  //          }
             
-            def create = false
-            try {
-                stage name: 'Packer: Create AMI', concurrency: 1
-                input message: 'Create AMI?', ok: 'Create'
-                create = true
-            } catch (err) {
-                println "Create AMI Discarded: ${env.JOB_NAME} - ${env.BUILD_NUMBER}"
-                create = false
-                currentBuild.result = 'UNSTABLE'
-            }
+ //           def create = false
+ //           try {
+ //               stage name: 'Packer: Create AMI', concurrency: 1
+ //               input message: 'Create AMI?', ok: 'Create'
+  //              create = true
+  //          } catch (err) {
+ //               println "Create AMI Discarded: ${env.JOB_NAME} - ${env.BUILD_NUMBER}"
+  //              create = false
+   //             currentBuild.result = 'UNSTABLE'
+  //          }
             
-            if (create) {
+  //          if (create) {
                 
-                if (fileExists("build.status")) {
-                    sh "rm -f build.status"
-                }
-                sh """(
-                    packer.io build -var-file=./variables.json  -var 'aws_access_key=${AWS_ID}' -var 'aws_secret_key=${AWS_KEY}' packer.json; echo \$? > build.status 
-                )"""
-            }
+  //              if (fileExists("build.status")) {
+  //                  sh "rm -f build.status"
+  //              }
+  //              sh """(
+  //                  packer.io build -var-file=./variables.json  -var 'aws_access_key=${AWS_ID}' -var 'aws_secret_key=${AWS_KEY}' packer.json; echo \$? > build.status 
+  //              )"""
+  //          }
         }
     }
 
